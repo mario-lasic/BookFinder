1d21a9bd81a4e8e5d414601e588e4b60c3083a1d
Mario Lasić <mario.lasic95@gmail.com>
2020-08-24 19:30:04 +0200

    Added second activity (ListViewActivity)

--- app/src/main/java/com/example/bookfinder/MainActivity.java
+++ app/src/main/java/com/example/bookfinder/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.bookfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnFind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initWidgets();
+        startNewActivity();
+    }
+
+    private void startNewActivity() {
+        final Intent intent = new Intent(this, ListViewActivity.class);
+        btnFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initWidgets() {
+        btnFind = (Button)findViewById(R.id.btnFind);
     }
 }
