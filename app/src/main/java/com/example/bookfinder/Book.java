af1bafeb189a28ca284053ef4491d8c2bd36ff37
Mario Lasić <mario.lasic95@gmail.com>
2020-08-27 13:01:17 +0200

    Added book object

---
+++ app/src/main/java/com/example/bookfinder/Book.java
@@ -0,0 +1,58 @@
+package com.example.bookfinder;
+
+public class Book {
+
+    private String mTitle;
+    private String mAuthor;
+    private String mImgLink;
+    private String mPreviewLink;
+    private String mDescription;
+
+    public Book(String title, String author, String imgLink, String previewLink, String description) {
+        mTitle = title;
+        mAuthor = author;
+        mImgLink = imgLink;
+        mPreviewLink = previewLink;
+        mDescription = description;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public void setAuthor(String author) {
+        mAuthor = author;
+    }
+
+    public String getImgLink() {
+        return mImgLink;
+    }
+
+    public void setImgLink(String imgLink) {
+        mImgLink = imgLink;
+    }
+
+    public String getPreviewLink() {
+        return mPreviewLink;
+    }
+
+    public void setPreviewLink(String previewLink) {
+        mPreviewLink = previewLink;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+}
